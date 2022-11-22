281db04ed6573c2a996acfa89d9068c5494e2e52
Norbert Kiesel <nk@iname.com>
2022-11-21 21:16:53 -0800

    fixed server pom

--- quarkus-client-mtls/src/main/kotlin/org/acme/client/mtls/GreetingResource.kt
+++ quarkus-client-mtls/src/main/kotlin/org/acme/client/mtls/GreetingResource.kt
@@ -1,6 +1,5 @@
 package org.acme.client.mtls
 
-import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -8,10 +7,7 @@ import javax.ws.rs.core.MediaType
 import org.eclipse.microprofile.rest.client.inject.RestClient
 
 @Path("/hello-client")
-class GreetingResource {
-        @Inject
-        @RestClient
-        lateinit var greetingService: GreetingService
+class GreetingResource(@RestClient private val greetingService: GreetingService) {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
